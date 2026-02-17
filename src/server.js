54e0ab1ed3479c29fedefbeba4392969606643ed
Maryam Sanni <sannimaryam134@gmail.com>
2026-02-17 19:54:15 +0100

    server.js

--- src/server.js
+++ src/server.js
@@ -2,4 +2,6 @@ import "dotenv/config";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
